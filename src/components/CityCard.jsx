3dd81dadca872d601f6b12789e7a7afdca442617
Valeyev Rustam <snooks87@gmail.com>
2018-11-07 23:57:06 +0300

    move body for city card to separate component

--- src/components/CityCard.jsx
+++ src/components/CityCard.jsx
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types'
 import Card from '@material-ui/core/Card'
 import CardActions from '@material-ui/core/CardActions'
 import CardContent from '@material-ui/core/CardContent'
-import CardMedia from '@material-ui/core/CardMedia'
 import Button from '@material-ui/core/Button'
 import Typography from '@material-ui/core/Typography'
 import LinearProgress from '@material-ui/core/LinearProgress'
@@ -13,10 +12,10 @@ import { withStyles } from '@material-ui/core/styles'
 import { connect } from 'react-redux'
 import { deleteCity, loadWeather } from '../store/actions'
 
-import WeatherInfo from './WeatherInfo'
+import CityCardBody from './CityCardBody'
 
 
-const styles = theme => ({
+const styles = () => ({
   card: {
     height: '100%',
     display: 'flex',
@@ -29,27 +28,6 @@ const styles = theme => ({
     marginLeft: 'auto'
   }
 })
-
-const CardBody = ({ weather, error }) => {
-  if (error)  {
-    let text = 'Error while loading data'
-    
-    if (error.response.status === 404) text = 'No data for this location'
-
-    return <Typography color='error'>{text}</Typography>
-  }
-
-  if (weather) {
-    return <WeatherInfo weather={weather} />
-  }
-
-  return null
-}
-
-CardBody.propTypes = {
-  weather: PropTypes.object,
-  error: PropTypes.object
-}
 
 class CityCard extends Component {
   componentDidMount() {
@@ -73,7 +51,7 @@ class CityCard extends Component {
           {loading ? (
             <LinearProgress />
           ) : (
-            <CardBody weather={weather} error={error}/>
+            <CityCardBody weather={weather} error={error}></CityCardBody>
           )}
         </CardContent>
         <CardActions>
---
+++ src/components/CityCardBody.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Typography from '@material-ui/core/Typography'
+import { withStyles } from '@material-ui/core/styles'
+
+
+import WeatherInfo from './WeatherInfo'
+
+const styles = theme => ({
+  caption: {
+    marginTop: theme.spacing.unit,
+    textAlign: 'right'
+  }
+})
+
+const CityCardBody = ({ weather, error, classes }) => {
+  if (error)  {
+    let text = 'Error while loading data'
+    
+    if (error.response.status === 404) text = 'No data for this location'
+
+    return <Typography color='error'>{text}</Typography>
+  }
+
+  if (weather) {
+    return (
+      <div>
+        <WeatherInfo weather={weather} />
+        <Typography variant="caption" className={classes.caption}>{weather.dt}</Typography>
+      </div>
+    )
+  }
+
+  return null
+}
+
+CityCardBody.propTypes = {
+  weather: PropTypes.object,
+  error: PropTypes.object,
+  classes: PropTypes.object.isRequired,
+}
+
+export default withStyles(styles)(CityCardBody)
